b259ac83ffc43d24d83846e00fd17e6e8aa176eb
name <peraiahtellagorla92@gmail.com>
2026-08-12 16:09:27 +0530

    preparation

--- create-file.sh
+++ create-file.sh
@@ -2,3 +2,22 @@
  touch /home/ec2-user/color.txt
 echo " the file craeted"
 
+
+
+touch test.txt
+
+if [ -f "test.txt" ];
+then
+echo "it is a regular a file"
+else 
+    echo "it is not regular file"
+fi
+
+
+touch data.txt
+if [ -e "data.txt" ];
+then
+    echo "file exist"
+else
+    echo "file doesn,t exit"
+fi
